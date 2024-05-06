9dee95abe26481bf1126f2bd53a25a822cbb6bc8
aman1108 <shrivastava250@gmail.com>
2024-05-06 22:48:58 +0530

    'checkpoint-3'

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -1,8 +1,12 @@
 
 export default function ToDoList() {
+    const tasks = ["Read SpringBoot", "Complete Assignments", "Prepare Breakfast", "Sleep for 2 hours", "Take a shower"];
+
     return(
         <ul>
-            <li>Nothing to do buddy, Sleep!!</li>
+            {tasks.map((task, index) => (
+                <li key={index}>{task}</li>
+            ))}
         </ul>
     );
 }
