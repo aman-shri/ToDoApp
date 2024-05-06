fd2b5fcaa3c3634b36ad7c78d61c86f39897cb39
aman1108 <shrivastava250@gmail.com>
2024-05-06 23:02:22 +0530

    'checkpoint-4'

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -1,12 +1,30 @@
+import React from "react";
+import { useState } from "react";
 
 export default function ToDoList() {
-    const tasks = ["Read SpringBoot", "Complete Assignments", "Prepare Breakfast", "Sleep for 2 hours", "Take a shower"];
+    const [tasks, setTasks] = useState(["Read SpringBoot", "Complete Assignments", "Prepare Breakfast", "Sleep for 2 hours", "Take a shower"]);
 
-    return(
-        <ul>
-            {tasks.map((task, index) => (
+    const handleEmptyClick = () => {
+        setTasks([]);
+    };
+
+    if (tasks.length === 0) {
+        return (
+          <div>
+            <p style={{ fontStyle: 'italic' }}>Nothing to do buddy. Sleep!</p>
+            <button onClick={handleEmptyClick}>Empty</button>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <ul>
+              {tasks.map((task, index) => (
                 <li key={index}>{task}</li>
-            ))}
-        </ul>
-    );
+              ))}
+            </ul>
+            <button onClick={handleEmptyClick}>Empty</button>
+          </div>
+        );
+      }
 }
